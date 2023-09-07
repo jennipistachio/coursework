e88283a1e0b51821c7b412d1d15438644141fea1
jennipistachio <142184371+jennipistachio@users.noreply.github.com>
2023-09-07 13:49:39 -0700

    Add files via upload

---
+++ lock.js
@@ -0,0 +1,17 @@
+/*
+Make a dialog box with html that uses javascrpt code
+that displays a message and the 3 number code for the vault
+*/
+
+let vaultNum1 = 5 + 5;
+let vaultNum2 = 2 * 20;
+let vaultNum3 = 40 - 1;
+
+console.log("You have recieved this message because you have been choosen to open an important vault.");
+console.log("Here is the secret combination:");
+
+console.log("[" + vaultNum1 + "-" + vaultNum2 + "-" + vaultNum3 + "]");
+
+
+//create a dialog box that shows the vault code usng html so search on youtube how to use js and html together 
+// so it should be displaying the message above and then the vault number combo
