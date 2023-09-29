4a806a82ccb1f594a30f3246171ae93ccc8f0434
jennipistachio <jennilynvillalba@gmail.com>
2023-09-29 12:05:27 -0700

    added and edited files

---
+++ block13/test.js
@@ -0,0 +1,20 @@
+/* if(1) {
+    console.log("truthy");
+
+} 
+
+else {
+    console.log("falsy");
+} */
+
+const isOver26 = true;
+const hasTicket = true;
+
+if(isOver26){
+    console.log("over 26");
+}
+else if(hasTicket){
+    console.log("has ticket");
+} else{
+    console.log("under 26");
+}
